bad3d497ba17fbdbd5dfe9d436604abe10fafcf8
Parizoda-max <sotiboldiyevaparizoda0@gmail.com>
2026-07-18 13:43:04 +0500

    Nimadi

--- src/components/about1/brand/Brand.jsx
+++ src/components/about1/brand/Brand.jsx
@@ -1,5 +1,12 @@
-import "./Brand.css";
-function Brand() {
-  return <div></div>;
+import "./Banner.css";
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <h2 className="banner-title">
+        A brand built on the love of craftmanship, quality and outstanding
+        customer service
+      </h2>
+    </div>
+  );
 }
-export default Brand;
