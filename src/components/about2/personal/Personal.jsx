8ea85f2d367015b0cadc15465fb54d7660958bcd
Muhammadali Dadaxanov <dadaxanovmuhammadali17@gmail.com>
2026-07-18 13:42:28 +0500

    about2

--- src/components/about2/personal/Personal.jsx
+++ src/components/about2/personal/Personal.jsx
@@ -1,9 +1,17 @@
 import "./Personal.css"
+import startedImage from "./started.svg"
 function Personal(){
   return (
-    <div>
-
-    </div>
+      <div className="started">
+            <div className="started-left">
+                <p className="started-main-text">It started with a small idea</p>
+                <p className="started-second-text">A global brand with local beginnings, our story begain in a small studio in South London in early 2014</p> 
+                <button className="started-btn">View collection</button>
+            </div>
+            <div className="started-rigth">
+                <img src={startedImage} alt="" />
+            </div>
+        </div>
   )
 }
 export default Personal
