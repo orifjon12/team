2041a604c469e092d05d1e70e0a02d29cfb9c28b
orifjon12 <loxiya04@gmail.com>
2026-07-18 11:08:41 +0500

    boshlandi

--- src/router/about12/About12.jsx
+++ src/router/about12/About12.jsx
@@ -1,6 +1,16 @@
+import About2Makes from "../../components/about2/About2Makes/About2Makes";
+import Buil from "../../components/about2/buil/Buil";
+import Personal from "../../components/about2/personal/Personal";
+import Started from "../../components/about2/started/Started";
+
 function About12(){
   return (
-    <div className="home"></div>
+    <div className="home">
+      <Started/>
+      <Personal/>
+      <Buil/>
+      <About2Makes/>
+    </div>
   )
 }
 
--- src/router/home/Home.jsx
+++ src/router/home/Home.jsx
@@ -1,14 +1,10 @@
-import Banner from '../../components/home/banner/Banner'
 import Ceramic from '../../components/home/ceramic/Ceramic'
 import Makes from '../../components/home/makes/Makes'
-import Navbar from '../../components/home/navbar/Navbar'
 import Product from '../../components/home/product/Product'
 
 function Home(){
   return (
     <div className="home">
-      <Navbar/>
-      <Banner/>
       <Makes/>
       <Ceramic/>
       <Product/>
